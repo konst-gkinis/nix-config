bfda53af82089985d7883dbb2bb8f4b56417bf68
Konstantinos Gkinis <konst.gkinis@gmail.com>
2026-05-18 10:29:55 +0200

    Remove unwanted macos settings

--- darwin/default.nix
+++ darwin/default.nix
@@ -88,15 +88,7 @@ in
 
     defaults = {
       NSGlobalDomain = {
-        AppleShowAllExtensions = true;
-        ApplePressAndHoldEnabled = false;
-
-        KeyRepeat = 2; # Values: 120, 90, 60, 30, 12, 6, 2
-        InitialKeyRepeat = 15; # Values: 120, 94, 68, 35, 25, 15
-
         "com.apple.mouse.tapBehavior" = 1;
-        "com.apple.sound.beep.volume" = 0.0;
-        "com.apple.sound.beep.feedback" = 0;
       };
 
       dock = {
